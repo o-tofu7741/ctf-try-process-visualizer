bd394c918661e3eda6326435199af1c467826877
o-tofu <81277991+o-tofu7741@users.noreply.github.com>
2024-11-11 19:17:33 +0900

    Refactor log analyzer to return parsed data and integrate CWE-ID lookup

--- log-analyze.py
+++ log-analyze.py
@@ -3,13 +3,17 @@ from datetime import datetime
 from pprint import pprint
 
 
-def main() -> None:
+def main() -> list[dict]:
     # Path to the JSON file
-    file_path = "log/modsec_audit.json"
+    json_log_filepath = "log/modsec_audit.json"
+    json_cwe_filepath = "data/cwe-id-dict.json"
+
+    with open(json_cwe_filepath, "r", encoding="utf-8") as file:
+        cwe_dict = json.load(file)
 
     # Read and parse the JSON file
     data: list[dict] = []
-    with open(file_path, "r", encoding="utf-8") as file:
+    with open(json_log_filepath, "r", encoding="utf-8") as file:
         for line in file:
             data.append(json.loads(line))
 
@@ -41,14 +45,11 @@ def main() -> None:
                     "request": req,
                     "response": res,
                     "messages": list(map(parse_message, messages)),
+                    "cwe_id": find_cwe_id(messages[0], cwe_dict),
                 }
             )
 
-    # pprint(parsed_data)
-    # print(len(data), len(parsed_data))
-
-    # for d in parsed_data:
-    #     pprint(d.get("messages", []))
+    return parsed_data
 
 
 def parse_message(message: dict) -> dict:
@@ -56,6 +57,7 @@ def parse_message(message: dict) -> dict:
     message_match: str = message["details"]["match"]
     message_file: str = message["details"]["file"]
     message_tags: list[str] = message["details"]["tags"]
+
     return {
         "message": message_str,
         "match": message_match,
@@ -64,10 +66,29 @@ def parse_message(message: dict) -> dict:
     }
 
 
+def find_cwe_id(message: dict, cwe_dict: dict) -> str:
+    # Simple example: find the first CWE-ID that matches a keyword in the message
+    cwe_id_list: dict[str, int] = {}
+
+    for cwe in cwe_dict["cwe"]:
+        cwe_id = cwe["cwe-id"]
+        for keyword in cwe["alias"]:
+            for key, value in message.items():
+                if key is list:
+                    for item in value:
+                        if keyword in item:
+                            cwe_id_list[cwe_id] = cwe_id_list.get(cwe_id, 0) + 1
+                else:
+                    if keyword in value:
+                        cwe_id_list[cwe_id] = cwe_id_list.get(cwe_id, 0) + 1
+
+    return max(cwe_id_list) if cwe_id_list else "Unknown"
+
+
 def parse_date(date_str: str) -> datetime:
     date_format = "%a %b %d %H:%M:%S %Y"
     return datetime.strptime(date_str, date_format)
 
 
 if __name__ == "__main__":
-    main()
+    pprint(main())
