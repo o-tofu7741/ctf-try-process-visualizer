7a5774fdcaf96f9a98a532b45677b040a868feae
o-tofu <81277991+o-tofu7741@users.noreply.github.com>
2024-11-11 17:38:04 +0900

    Add log analyzer for ModSecurity audit JSON files

---
+++ log-analyze.py
@@ -0,0 +1,60 @@
+import json
+from datetime import datetime
+from pprint import pprint
+
+
+def main() -> None:
+    # Path to the JSON file
+    file_path = "log/modsec_audit.json"
+
+    # Read and parse the JSON file
+    data: list[dict] = []
+    with open(file_path, "r", encoding="utf-8") as file:
+        for line in file:
+            data.append(json.loads(line))
+
+    # Parse the data
+    parsed_data = []
+    for transaction in data:
+        try:
+            tr: dict = transaction["transaction"]
+            client_ip: str = tr["client_ip"]
+            time_stamp: str = tr["time_stamp"]
+
+            unique_id: str = tr["unique_id"]
+
+            req: dict = tr["request"]
+            res: dict = tr["response"]
+
+            messages: list[dict] = tr["messages"]
+            if len(messages) == 0:
+                raise Exception("No messages found")
+        except Exception as e:
+            # print(e)
+            continue
+        else:
+            parsed_data.append(
+                {
+                    "client_ip": client_ip,
+                    "time_stamp": parse_date(time_stamp),
+                    "unique_id": unique_id,
+                    "request": req,
+                    "response": res,
+                    "messages": messages,
+                }
+            )
+
+    # pprint(parsed_data)
+    print(len(data), len(parsed_data))
+
+    for d in parsed_data:
+        pprint(d.get("messages", []))
+
+
+def parse_date(date_str: str) -> datetime:
+    date_format = "%a %b %d %H:%M:%S %Y"
+    return datetime.strptime(date_str, date_format)
+
+
+if __name__ == "__main__":
+    main()
